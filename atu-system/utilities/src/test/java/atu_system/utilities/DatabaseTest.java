86631db1fa9e0d86c14740854473df1c8c1de937
Jaden Tse <hoichun1209@gmail.com>
2022-11-23 13:41:41 +0800

    Organise packages

--- atu-system/utilities/src/test/java/atu_system/utilities/DatabaseTest.java
+++ atu-system/utilities/src/test/java/atu_system/utilities/DatabaseTest.java
@@ -1,4 +1,4 @@
-package utilities;
+package atu_system.utilities;
 
 import static org.junit.Assert.*;
 
--- atu-system/utilities/src/test/java/atu_system/utilities/TeamTest.java
+++ atu-system/utilities/src/test/java/atu_system/utilities/TeamTest.java
@@ -1,4 +1,4 @@
-package utilities;
+package atu_system.utilities;
 
 import static org.junit.Assert.*;
 
@@ -24,6 +24,18 @@ public class TeamTest {
 		student3 = new Student("34563456", "Radi", 3, 4, false, true, false, null, 2);
 		student4 = new Student("45674567", "Kenneth", 5, 4, true, true, true, null, 3);
 	}
+	
+	public void assertStudentsEqual (Student s1, Student s2) {
+		assertEquals(s1.getId(), s2.getId());
+		assertEquals(s1.getName(), s2.getName());
+		assertEquals(s1.getK1Energy(), s2.getK1Energy());
+		assertEquals(s1.getK2Energy(), s2.getK2Energy());
+		assertEquals(s1.getK3Tick1(), s2.getK3Tick1());
+		assertEquals(s1.getK3Tick2(), s2.getK3Tick2());
+		assertEquals(s1.getLeaderPreference(), s2.getLeaderPreference());
+		assertEquals(s1.getConcerns(), s2.getConcerns());
+		assertEquals(s1.getRowID(), s2.getRowID());
+	}
 
 	@Test
 	public void testCreateEmptyTeam() {
@@ -33,7 +45,7 @@ public class TeamTest {
 	}
 	
 	@Test
-	public void testAppendMembers() {
+	public void testAppendMembersAndGetOneMember() {
 		Team team = new Team(1);
 		assertEquals(team.getId(), 1);
 		assertArrayEquals(team.getMembers(), new Student[4]);
@@ -55,6 +67,23 @@ public class TeamTest {
 		// Append the fifth member
 		team.appendMember(student1);
 		assertArrayEquals(team.getMembers(), new Student[] {student1, student2, student3, student4});
+		
+		// Test getOneMem
+		Student s = team.getOneMem(1);
+		assertStudentsEqual(s, student2);
+	}
+	
+	@Test
+	public void testSetupLeader() {
+		Team team = new Team(1);
+		team.appendMember(student1);
+		team.appendMember(student2);
+		team.appendMember(student3);
+		team.appendMember(student4);
+		
+		team.setupLeader();
+		
+		assertStudentsEqual(team.getLeader(), student2);
 	}
 
 }
